ecc6ca56e7faa16fedaa603185528072fbeaaa50
FluffyWeirdO <veronica.emelianova@gmail.com>
2016-06-22 21:41:14 +0300

    Removed browser initialization each time + test improvements

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -1,19 +1,25 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.List;
 
 public class GroupDeletionTests extends TestBase {
-    @Test
-    public void testGroupDeletion() {
+
+    @BeforeMethod
+    public void ensurePreconditions() {
         app.getNavigationHelper().gotoGroupPage();
-        List<GroupData> before = app.getGroupHelper().getGroupList();
         if (!app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("TestGroup", "TestHeader", "TestFooter"));
         }
+    }
+
+    @Test
+    public void testGroupDeletion() {
+        List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnToGroupPage();
