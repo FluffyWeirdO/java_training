a1f048f7c75eb24a32393c23617a79c41aa2593e
FluffyWeirdO <veronica.emelianova@gmail.com>
2016-06-26 15:12:33 +0300

    Homework 15 - data providers from file into tests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,27 +1,69 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
+    @DataProvider
+    public Iterator<Object[]> validContactsFromXml() throws IOException {
+        List<Object[]> list = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+        String line = reader.readLine();
+        String xml = "";
+        while (line != null) {
+            xml = xml + line;
+            line = reader.readLine();
+        }
+        XStream xStream = new XStream();
+        xStream.processAnnotations(ContactData.class);
+        List<ContactData> contacts = (List<ContactData>) xStream.fromXML(xml);
+        return contacts.stream().map((c) -> new Object[]{(c)}).collect(Collectors.toList()).iterator();
+    }
 
-    @Test
-    public void testContactCreation() {
+    @DataProvider
+    public Iterator<Object[]> validContactsFromJson() throws IOException {
+        List<Object[]> list = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+        String line = reader.readLine();
+        String json = "";
+        while (line != null) {
+            json = json + line;
+            line = reader.readLine();
+        }
+        Gson gson = new Gson();
+        List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>() {
+        }.getType());
+        return contacts.stream().map((c) -> new Object[]{(c)}).collect(Collectors.toList()).iterator();
+    }
+
+    @Test(dataProvider = "validContactsFromJson")
+    public void testContactCreation(ContactData contact) {
         Contacts before = app.contact().all();
         app.goTo().contactPage();
-        File photo = new File("src/test/resources/avatar.jpg");
-        ContactData contact = new ContactData().withFirstName("TestName").withMiddleName("TestMiddleName")
-                .withLastName("TestLastName").withNickname("TestNickname").withTitle("Mr").withCompany("TestCompany")
-                .withAddress("TestAddress1").withHomePhone("+380444759784").withMobilePhone("+380634873365")
-                .withWorkPhone("+380448764561").withBirthDate("10").withBirthMonth("January")
-                .withBirthYear("1988").withEmail1("test1@gmail.com").withEmail2("test2@gmail.com").withEmail3("test3@gmail.com")
-                .withPhoto(photo);
+//        File photo = new File("src/test/resources/avatar.jpg");
+//        ContactData contact = new ContactData().withFirstName("TestName").withMiddleName("TestMiddleName")
+//                .withLastName("TestLastName").withNickname("TestNickname").withTitle("Mr").withCompany("TestCompany")
+//                .withAddress("TestAddress1").withHomePhone("+380444759784").withMobilePhone("+380634873365")
+//                .withWorkPhone("+380448764561").withBirthDate("10").withBirthMonth("January")
+//                .withBirthYear("1988").withEmail1("test1@gmail.com").withEmail2("test2@gmail.com").withEmail3("test3@gmail.com")
+//                .withPhoto(photo);
 
         app.contact().create(contact);
 
