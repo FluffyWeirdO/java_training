8fe63fd83195b21b541d86fa7c91546e56d3df2a
FluffyWeirdO <veronica.emelianova@gmail.com>
2016-06-28 22:44:17 +0300

    Added logging using logback lib

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,13 +1,22 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class TestBase {
     protected static final ApplicationManager app
             = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+
+    Logger logger = LoggerFactory.getLogger(GroupCreationTests.class);
 
     @BeforeSuite
     public void setUp() throws Exception {
@@ -17,5 +26,15 @@ public class TestBase {
     @AfterSuite
     public void tearDown() {
         app.stop();
+    }
+
+    @BeforeMethod(alwaysRun = true)
+    public void logTestStart(Method m, Object[] o) {
+        logger.info("Start test" + m.getName() + " with parameters " + Arrays.asList(o));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void logTestStop(Method m) {
+        logger.info("Stop test" + m.getName());
     }
 }
