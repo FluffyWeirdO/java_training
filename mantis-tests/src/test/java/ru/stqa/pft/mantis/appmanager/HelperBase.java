6c4dc88a45afd15e7adce08663bfb761a808fcf4
FluffyWeirdO <veronica.emelianova@gmail.com>
2016-07-21 22:24:22 +0300

    FTP connection and Mail delivery

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HelperBase.java
@@ -0,0 +1,68 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+
+public class HelperBase {
+
+    protected WebDriver wd;
+    protected ApplicationManager app;
+
+    public HelperBase(ApplicationManager app) {
+        this.app = app;
+        this.wd = app.getDriver();
+    }
+
+    protected void click(By locator) {
+        wd.findElement(locator).click();
+    }
+
+    protected void type(By locator, String text) {
+        click(locator);
+        if (text != null) {
+            String existingText = wd.findElement(locator).getAttribute("value");
+            if (!text.equals(existingText)) {
+                wd.findElement(locator).clear();
+                wd.findElement(locator).sendKeys(text);
+            }
+        }
+    }
+
+    protected void attach(By locator, File file) {
+        if (file != null) {
+            wd.findElement(locator).sendKeys(file.getAbsolutePath());
+        }
+    }
+
+    public boolean isAlertPresent() {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    public void selectFromDropDownList(By selectLocator, String optionText) {
+        new Select(wd.findElement(selectLocator)).selectByVisibleText(optionText);
+    }
+
+    protected boolean isElementPresent(By locator) {
+        try {
+            wd.findElement(locator);
+            return true;
+        } catch (NoSuchElementException ex) {
+            return false;
+        }
+    }
+
+    public void waitForPageRefresh(By locator) {
+        WebDriverWait wait = new WebDriverWait(wd, 10);
+        WebElement element = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+}
