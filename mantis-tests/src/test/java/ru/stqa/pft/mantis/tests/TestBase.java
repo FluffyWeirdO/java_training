1783da374f6bb580194fa01d906ead770f9ac873
FluffyWeirdO <veronica.emelianova@gmail.com>
2016-07-23 13:57:59 +0300

    Homework 21 - Mantis integration

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -1,12 +1,17 @@
 package ru.stqa.pft.mantis.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.testng.SkipException;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
+import ru.stqa.pft.mantis.model.Issue;
 
+import javax.xml.rpc.ServiceException;
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
 
 public class TestBase {
     protected static final ApplicationManager app
@@ -23,5 +28,17 @@ public class TestBase {
     public void tearDown() throws IOException {
         app.ftp().restore("config_inc.php.bak", "config_inc.php");
         app.stop();
+    }
+
+    public boolean isIssueOpen(int issueId) throws MalformedURLException, ServiceException, RemoteException {
+        Issue issue = app.soap().getIssue(issueId);
+        String status = issue.getStatus();
+        return !status.equals("closed");
+    }
+
+    public void skipIfNotFixed(int issueId) throws RemoteException, ServiceException, MalformedURLException {
+        if (isIssueOpen(issueId)) {
+            throw new SkipException("Ignored because of issue: " + issueId);
+        }
     }
 }
